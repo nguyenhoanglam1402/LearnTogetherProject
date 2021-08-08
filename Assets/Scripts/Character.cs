efa64b559c322f74989e13b3e483155403199aba
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-08-08 18:11:24 +0700

    feat(character): added character class to control character movement

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,18 +2,23 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+  private float speed = 10.0f;
+  private Joystick joystick;
   // Start is called before the first frame update
   void Start()
   {
+    joystick = FindObjectOfType<Joystick>();
   }
 
   // Update is called once per frame
   void Update()
   {
-  }
-
-  void MoveFoward()
-  {
-
+    var rigidbody = GetComponent<Rigidbody>();
+    if (joystick.Horizontal > 0 || joystick.Horizontal < 0 ||
+    joystick.Vertical > 0 || joystick.Vertical < 0)
+    {
+      transform.Translate(Vector3.right * joystick.Horizontal * Time.deltaTime * speed);
+      transform.Translate(Vector3.forward * joystick.Vertical * Time.deltaTime * speed);
+    }
   }
 }
